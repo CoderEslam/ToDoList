b9a542143bd4046da359bb20a777fd5386ab4b88
CoderEslam <eslamghazy600@gmail.com>
2023-07-31 19:39:17 +0200

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Eslam Ghazy on 31/7/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
